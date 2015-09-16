c10c9732080e8403e5f584cec51c5bf4f2838d8d
collin <collin.inbox@gmail.com>
2015-09-15 20:17:40 -0400

    Initial Commit

---
+++ Push Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Push Test
+//
+//  Created by Collin Thomas on 9/15/15.
+//  Copyright (c) 2015 WDZ LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
